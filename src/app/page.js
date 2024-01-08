f3a2d51a8dd9737b78944f7bbface172f2aa4f5c
JustAnatoliyDev <119749324+Sskame1@users.noreply.github.com>
2024-01-08 18:29:59 +0300

    upd

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from '../style/page.module.scss'
 
 import decore from '@/img/Decore.png'
+import decore_sm from '@/img/Decore-sm.png'
 
 import Welcome from '@/components/Welcome'
 import Category from '@/components/Category'
@@ -13,6 +14,7 @@ import Footer from '@/components/Footer'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image src={decore_sm} className={styles.Decore_sm}/>
       <Image src={decore} className={styles.Decore}/>
       <Welcome />
       <Category />
